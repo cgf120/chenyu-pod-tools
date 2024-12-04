9b8da14b4e55726a8ef0009ecc1f32c3be714f94
CuiGaofeng <2381984@qq.com>
2024-12-04 14:21:55 +0800

    Update config.py

--- pod_model_manager/app/config.py
+++ pod_model_manager/app/config.py
@@ -1,4 +1,4 @@
 # 数据库配置
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cpnworker12349876@192.168.200.8:3306/pod-manager'
+SQLALCHEMY_DATABASE_URI = 'xxxx'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 MODEL_BASE_DIR = "/mnt/chenyu-nvme"
